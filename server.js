bbe010581b4c2eb601ffe9033363e47efa8aeb9b
ashwin-sekar <ashwinsekar14@gmail.com>
2021-05-22 17:35:42 +0530

    added client and connected to backend

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-const port = 5000;
+//middlewares
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req,res) => {
+app.get('/api', (req,res) => {
     const start = "Front-end and backend connected";
 
-    res.json(start);
-})
+    return res.json(start);
+});
+
+const port = 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

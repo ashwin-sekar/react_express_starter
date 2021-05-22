63cda26f9e3fbb2a7b3b4244297ac304fdf4f781
ashwin-sekar <ashwinsekar14@gmail.com>
2021-05-22 17:48:44 +0530

    api text displayed

--- client/src/App.js
+++ client/src/App.js
@@ -23,16 +23,8 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {connected}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
